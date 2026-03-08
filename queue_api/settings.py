58dbe65933cd4047b381da7e6c138241bda2ec6e
Michael <michaelkyalor@gmail.com>
2026-03-08 23:56:18 +0300

    fixed redis error

--- queue_api/settings.py
+++ queue_api/settings.py
@@ -73,18 +73,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'queue_api.wsgi.application'
 ASGI_APPLICATION = 'queue_api.asgi.application'
 
-# Redis channel layer
+# Channel layer
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.InMemoryChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+            },
 }
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+#CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 
 # Database
